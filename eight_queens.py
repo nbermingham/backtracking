3c2f4baceba3dc3571af8bd35175795abb7582e4
Nick Bermingham <nick.bermingham@faraday.io>
2021-09-29 01:10:23 -0400

    testing

---
+++ eight_queens.py
@@ -0,0 +1,8 @@
+class EightQueens:
+
+    def print_name(self, first_name: str, last_name: str) -> str:
+        return f"Hi, my name is {first_name} {last_name}"
+    
+
+eq = EightQueens()
+print(eq.print_name("nick", "bermingham"))
