05ebc130b03374693fc6af88663012649a1d97cd
Nick Bermingham <nick.bermingham@faraday.io>
2021-10-06 09:48:47 -0400

    second set of changes

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Backtracking
 
 This is a repo for Nick's backtracking algorithm problems.
+
+
+
--- eight_queens.py
+++ eight_queens.py
@@ -1,8 +1,16 @@
 class EightQueens:
 
-    def print_name(self, first_name: str, last_name: str) -> str:
+      def print_name(self, first_name: str, last_name: str) -> str:
+            """
+            function that prints your first and last name
+            """
         return f"Hi, my name is {first_name} {last_name}"
-    
+
+      def add_two_numbers(self, number_one: int, number_two: int) -> int:
+            """
+            function that adds two numbers together
+            """
+        return number_one + number_two
 
 eq = EightQueens()
 print(eq.print_name("nick", "bermingham"))
