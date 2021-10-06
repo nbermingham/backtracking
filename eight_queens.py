0a503711067d9c2b0d3372dab32dc2c5d05552a0
Nick Bermingham <nick.bermingham@faraday.io>
2021-10-06 10:54:10 -0400

    method work

--- eight_queens.py
+++ eight_queens.py
@@ -1,18 +1,24 @@
 class EightQueens:
 
-      def print_name(self, first_name: str, last_name: str) -> str:
-            """
-            function that prints your first and last name
-            """
-        return f"Hi, my name is {first_name} {last_name}"
+    def __init__(self, first_name: str, last_name: str):
+        self.first_name = first_name
+        self.last_name = last_name
 
-      def add_two_numbers(self, number_one: int, number_two: int) -> int:
-            """
-            function that adds two numbers together
-            """
+    
+    def print_name(self):
+        """
+        function that prints your first and last name
+        """
+        print(f"Hi, my name is {self.first_name} {self.last_name}")
+
+    @staticmethod
+    def add_two_numbers(number_one: int, number_two: int) -> int:
+        """
+        function that adds two numbers together
+        """
         return number_one + number_two
 
-eq = EightQueens()
-print(eq.print_name("nick", "bermingham"))
+eq = EightQueens("nick", "bermingham")
+eq.print_name() 
 
 #adding in some comments
