df3eff1dbb72056941ba7fcf4a88418fb5e8e554
Nick Bermingham <nick.bermingham@faraday.io>
2021-10-12 10:43:36 -0400

    rerun

--- eight_queens.py
+++ eight_queens.py
@@ -1,82 +1,72 @@
-# New file containing 8 queens backtracking problem
+"""
+backtracking algorithm to solve eight queens problem
+"""
+
 from typing import List
+
 
 def print_board(board: List[List[int]]):
     """
-    prints out the entire board
+    prints out board
     """
     for i in range(len(board)):
-        print(board[i])
+        print(*board[i], sep=" ")
 
-def is_safe(board: List[List[int]], row: int, col: int) -> bool:
+
+def solve_nq_til(board: List[List[int]], col: int, n: int) -> bool:
     """
-    check the row, column and diagonals to see if another queen is there
+    recursively places queens until all are placed
     """
-
-    n = len(board)
-
-    #checks the row
-    for i in range(col):
-        if board[row][i]:
-            return False
-
-    #checks upper diagonal on the left side
-    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
-        if board[i][j]:
-            return False
-
-    #checks lower diagonal on the left side
-    for i, j in zip(range(row, n, 1), range(col, n, 1)):
-        if board[i][j]:
-            return False
-
-    return True
-
-def solve_nq_til(board, col) -> bool:
-    """
-    solves
-    """
-
-    #base case: we've already placed all the queens 
-    if col >= N:
+    if col >= n:
         return True
-
     for i in range(N):
-        if is_safe(board, i, col):
+        if is_valid(board, i, col):
             board[i][col] = 1
-            if solve_nq_til(board, col + 1):
+            if solve_nq_til(board, col + 1, n):
                 return True
-
             board[i][col] = 0
 
     return False
 
+def is_valid(board: List[List[int]], row: int, col: int) -> bool:
+    """
+    checks to see placing the queen at row, col will make a valid board
+    """
+    n = len(board)
+    # checks all the tiles in the column
+    for i in range(n):
+        if board[i][col]:
+            return False
+        if board[row][i]:
+            return False
 
-def solve_nq(board: List[List[int]], int) -> bool:
+    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+        if board[i][j]:
+            return False
+
+    return True
+
+def solve_nq(board: List[List[int]], n: int) -> bool:
     """
     solves
     """
-    if not solve_nq_til(board, 0):
-        print("Solution does not exist")
-        return False
+    if solve_nq_til(board, 0, n):
+        print_board(board)
+        return True
 
-    print_board(board)
-    return True
-
+    print("No solution!")
+    return False
 
 if __name__ == "__main__":
-    N = 8
+    N = 5
 
-    board_two = [[0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0]]
+    BOARD = [[0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0]]
 
-
-    # print(board_two)
-
-    solve_nq(board_two, N)
+    solve_nq(BOARD, N)
