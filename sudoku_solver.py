5f575564b2fe6f2e87ff6171ae6f4ad184d9dcf7
Nick Bermingham <nick.bermingham@faraday.io>
2021-10-12 11:03:17 -0400

    start sudoku solver

---
+++ sudoku_solver.py
@@ -0,0 +1,19 @@
+"""
+solves a given sudoku solver given a 9 by 9 board
+"""
+from eight_queens import print_board
+
+
+
+if __name__ == "__main__":
+    board = [[0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0]]
+    print_board(board)
+    print("hello")
