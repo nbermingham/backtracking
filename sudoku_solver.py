925bf666a1cc6e12b594825f4aa42e8c1cc869b1
Nick Bermingham <nick.bermingham@faraday.io>
2021-10-13 11:00:02 -0400

    changed valid checker

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -4,28 +4,36 @@ solves a given sudoku solver given a 9 by 9 board
 from eight_queens import print_board
 from typing import List
 
-def solve_board_til(board: List[List[int]], presets: set(int), curr: int, val: int) -> bool:
+def find_next_tile(board: List[List[int]]) -> tuple(int, int):
+    """
+    finds the next tile with no number in it
+    """
+    for i in range(N):
+        for j in range(N):
+            if not board[i][j]:
+                return (i, j)
+    
+    return None
+
+
+def solve_board_til(board: List[List[int]], curr: int, val: int) -> bool:
     """
     recursively solves board
     """
-
-    if val > 8:
-        return False
-    
-    if curr >= N**2:
+    next_tile = find_next_tile(board)
+    if not next_tile:
         return True
+    else:
+        row, col = next_tile
 
-    # if current coordinates is in presets, go to next coordinate
-    if curr in presets:
-        return solve_board_til(board, presets, curr + 1, 1)
     
     row, col = divmod(curr, N)
-    if is_valid(board, (row, col), val):
+    if is_valid(board, tuple(row, col), val):
         board[row][col] = val
-        if solve_board_til(board, presets, curr+1, 1):
+        if solve_board_til(board, curr+1, 1):
             return True
         
-        solve_board_til(board, presets, curr, val+1)
+        solve_board_til(board, curr, val+1)
 
     
 
@@ -49,11 +57,7 @@ def solve_board(board: List[List[int]]) -> bool:
     """
     solves the board
     """
-    presets = ()
-    for i in range(N):
-        for j in range(N):
-            presets.add(9*i+j)
-    if solve_board_til(board, presets, 0, 1):
+    if solve_board_til(board, 0, 1):
         print_board(board)
         return True
     print("no solution") 
@@ -65,64 +69,24 @@ def is_valid(board: List[List[int]], coords: tuple(int, int), num: int) -> bool:
     checking if the row, column, and 3x3 tile does not already contain num
     """
     (row, col) = coords
-    # checks the row and the column to see if num already exists
+    # checks the row to see if num already exists
     for i in range(N):
-        if board[row][i] == num or board[i][col] == num:
+        if board[row][i] == num and (row, i) != coords:
             return False
 
-    if 0 <= row <= 2 and 0 <= col <= 2:
-        for i in range(0, 3):
-            for j in range(0, 3):
-                if board[i][j] == num:
-                    return False
-        
-    if 0 <= row <= 2 and 3 <= col <= 5:
-        for i in range(0, 3):
-            for j in range(2, 5):
-                if board[i][j] == num:
-                    return False
-    
-    if 0 <= row <= 2 and 6 <= col <= 8:
-        for i in range(0, 3):
-            for j in range(6, 8):
-                if board[i][j] == num:
-                    return False
+    # checks the column to see if num already exists
+    for i in range(N):
+        if board[i][col] == num and (i, col) != coords:
+            return False
 
-    if 3 <= row <= 5 and 0 <= col <= 2:
-        for i in range(3, 5):
-            for j in range(0, 3):
-                if board[i][j] == num:
-                    return False
 
-    if 3 <= row <= 5 and 3 <= col <= 5:
-        for i in range(3, 5):
-            for j in range(3, 5):
-                if board[i][j] == num:
-                    return False
+    row_box = row // 3
+    col_box = col // 3
 
-    if 3 <= row <= 5 and 6 <= col <= 8:
-        for i in range(3, 5):
-            for j in range(6, 8):
-                if board[i][j] == num:
-                    return False
-
-    if 6 <= row <= 8 and 0 <= col <= 2:
-        for i in range(6, 8):
-            for j in range(0, 3):
-                if board[i][j] == num:
-                    return False
-
-    if 6 <= row <= 8 and 3 <= col <= 5:
-        for i in range(0, 3):
-            for j in range(0, 3):
-                if board[i][j] == num:
-                    return False
-
-    if 6 <= row <= 8 and 6 <= col <= 8:
-        for i in range(0, 3):
-            for j in range(0, 3):
-                if board[i][j] == num:
-                    return False
+    for i in range(row_box*3, row_box*3 + 3):
+        for j in range(col_box*3, col_box*3 + 3):
+            if board[i][j] == num and (i, j) != coords:
+                return False
 
     return True
 
